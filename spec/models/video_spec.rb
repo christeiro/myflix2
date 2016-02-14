5fd150e933c9b16f404289aa84738959d0717699
Krister Vals <krister.vals@gmail.com>
2016-02-14 19:42:17 +0200

    Initial category and video model tests

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
 
 describe Video do
+  it { should belong_to(:category) }
   it "saves itself" do
     video = Video.new(title: "monk", description: "Funny video")
     video.save
     expect(Video.first).to eq(video)
+  end
+
+  it "belongs to category" do
+    dramas = Category.create(name: "Dramas")
+    monk = Video.create!(title: "Monk", description: "Monk description", category: dramas)
+    expect(monk.category).to eq(dramas)
   end
 end
