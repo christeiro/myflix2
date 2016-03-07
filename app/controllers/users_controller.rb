c06c76a086390c5fe3437e032238e99aabebf0e1
Krister Vals <krister.vals@gmail.com>
2016-03-07 22:24:53 +0200

    User registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  before_action :set_params, only: [:create]
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(set_params)
+    if @user.save
+      redirect_to sign_in_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def set_params
+    params.require(:user).permit(:email, :full_name, :password)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Myflix::Application.routes.draw do
-  get 'ui(/:action)', controller: 'ui'
+  root to: "pages#front"
   get '/home', to: 'videos#index'
   resources :videos, only: [ :show, :search ] do
     get 'search', on: :collection
   end
   resources :categories, only: [ :show ]
+  get 'ui(/:action)', controller: 'ui'
+  get 'register', to: "users#new"
+  get 'sign_in', to: "sessions#new"
+  resources :users, only: [ :create ]
 end
