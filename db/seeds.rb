050eb3f958706699e289fc18afe29d7c35db054c
Krister Vals <krister.vals@gmail.com>
2016-03-07 21:19:29 +0200

    Recent 6 videos per category

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,3 +12,10 @@ dramas = Category.create!(name: "TV Dramas")
 southpark = Video.create!(title: "Southpark", description: "Description of Southpark", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg", category: cartoons)
 futurama = Video.create!(title: "Futurama", description: "Description of Futurama", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: cartoons)
 monk = Video.create!(title: "Monk", description: "Description of Monk", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+
+batman = Video.create!(title: "Batman", description: "Description of Batman", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg", category: cartoons)
+robin = Video.create!(title: "Robin", description: "Description of Robin", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: cartoons)
+hospital = Video.create!(title: "Hospital", description: "Description of Hospital", small_cover_url: "/tmp/monk.jpg", large_cover_url: "/tmp/monk_large.jpg", category: dramas)
+
+viking = Video.create!(title: "Viking", description: "Description of Viking", small_cover_url: "/tmp/south_park.jpg", large_cover_url: "/tmp/south_park.jpg", category: cartoons)
+mouse = Video.create!(title: "Mouse", description: "Description of Mouse", small_cover_url: "/tmp/futurama.jpg", large_cover_url: "/tmp/futurama.jpg", category: cartoons)
