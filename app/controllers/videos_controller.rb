785015615b35ae7d1d7cd8f2e10527f183d63ec2
Krister Vals <krister.vals@gmail.com>
2016-03-07 20:45:33 +0200

    Search page

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,11 +1,14 @@
 class VideosController < ApplicationController
-
   def index
     @categories = Category.all
   end
 
   def show
     @video = Video.find(params[:id])
+  end
+
+  def search
+    @results = Video.search_by_title(params[:search_term])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Myflix::Application.routes.draw do
   get 'ui(/:action)', controller: 'ui'
   get '/home', to: 'videos#index'
-  resources :videos, only: [ :show ]
+  resources :videos, only: [ :show, :search ] do
+    get 'search', on: :collection
+  end
   resources :categories, only: [ :show ]
 end
