b6f527bc7044815827198d8989c5f451b08f54ff
Krister Vals <krister.vals@gmail.com>
2016-03-17 00:21:53 +0200

    Changes in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ class SessionsController < ApplicationController
   
   def create
     user = User.find_by(email: params[:email])
-    binding.pry
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       redirect_to home_path, notice: "Your are signed in, enjoy!"
